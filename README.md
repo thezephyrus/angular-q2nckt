0c2ead4f13d8af6dccc4693623c710b3d1f8feaa
thezephyrus <neyo_napster@yahoo.com>
2020-10-15 23:03:39 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-q2nckt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-q2nckt)
---
+++ src/app/products.ts
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    name: "Phone XL",
+    price: 799,
+    description: "A large phone with one of the best screens"
+  },
+  {
+    name: "Phone Mini",
+    price: 699,
+    description: "A great phone with one of the best cameras"
+  },
+  {
+    name: "Phone Standard",
+    price: 299,
+    description: "Stay connected"
+  },
+  {
+    name: "Phone 11",
+    price: 799,
+    description: "11th gen"
+  },
+  {
+    name: "Phone 11 Pro",
+    price: 1299,
+    description: "11th gen pro"
+  },
+  {
+    name: "Phone 11 Pro Max",
+    price: 2099,
+    description: "The best 11th gen"
+  }
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
